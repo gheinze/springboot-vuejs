ffc85609e99a3d7166920e7cccbac71c425561f7
Glenn <1091512+gheinze@users.noreply.github.com>
2020-04-13 13:28:32 -0400

    Springboot + vuejs start point

---
+++ backend/src/main/java/space/redoak/am/AmApplication.java
@@ -0,0 +1,13 @@
+package space.redoak.am;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/space/redoak/am/AmApplicationTests.java
@@ -0,0 +1,13 @@
+package space.redoak.am;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
